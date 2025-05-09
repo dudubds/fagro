4dc82dd9f83eaab076c768301678bbabe16bd561
dudubds <dudubds26@gmail.com>
2025-05-09 19:46:17 -0300

    teste

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+
+
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+      <Text>Vai aparcerecer no cell?</Text>
+    </View>
+  )
+}
